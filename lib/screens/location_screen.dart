21d20ee72274a079afa080818fe0e06cd4290dd8
LaylaKh <layla.khdva@gmail.com>
2023-12-20 15:52:05 +0300

    first

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import '../api/weather_api.dart';
+import 'weather_forecast_screen.dart';
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({Key? key}) : super(key: key);
+
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  void getLocationData() async {
+    try {
+      var weatherInfo = await WeatherApi().fetchWeatherForecast();
+      // ignore: use_build_context_synchronously
+      Navigator.push(context, MaterialPageRoute(builder: (context) {
+        return WeatherForecastScreen(locationWeather: weatherInfo);
+      }));
+    } catch (e) {
+      // print('$e');
+      log('$e');
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(color: Colors.black87, size: 100.0),
+      ),
+    );
+  }
+}
